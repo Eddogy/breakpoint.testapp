9d6aef16933945a319c33930c0c3018f2e07c9e3
Edgaras <erainius@yahoo.co.uk>
2021-01-24 01:06:08 +0200

    Class2 Added
    Pridejau Class2 Ir komentara testuoju

---
+++ BreakpointTestApp/BreakpointTestApp.Infrastructure/Class2.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BreakpointTestApp.Infrastructure
+{
+    class Class2
+    {
+        // Edgaras Testuoju
+    }
+}
